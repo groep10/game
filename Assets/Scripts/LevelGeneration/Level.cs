9403b80088c513ae81bf9abb086585f920b7bff0
janceesvansenden <jancees_van_senden@hotmail.com>
2014-11-19 17:33:43 +0100

    Created basic checkpoint

--- Assets/Scripts/LevelGeneration/Level.cs
+++ Assets/Scripts/LevelGeneration/Level.cs
@@ -7,22 +7,39 @@ public class Level : MonoBehaviour {
 	public int xResolution;
 	public int zResolution;
 	public bool makeLarger;
-	public float radius = 200;
+	public float terrainRadius = 200;
+	public GameObject checkpoint;
+
+	private float checkpointTimer = 5;
 
 	void Start(){
 		// creates an arena terrain with radius 200
 		editTerrain ();
+		Invoke ("setCheckpoint", 5);
 	}
 
 	// Use this for initialization
 	void Update () {
-		// makes the arena subsequently smaller and larger with a radius ranging between 150 and 200
-		adjustTerrainRadius ();
 	}
 
-	void FixedUpdate(){
+	// sets the checkpoint in the arena
+	void setCheckpoint(){
+		float x = Random.Range (-300, 300);
+		float z = Random.Range (-300, 300);
+		Vector3 location = new Vector3(x, 50f, z);
+		GameObject cpnt = (GameObject) Instantiate (checkpoint, location, Quaternion.identity);
+		Destroy (cpnt, checkpointTimer);
+		Invoke("setCheckpoint", checkpointTimer);
 	}
 
+//	// destroys the current checkpoint cpnt and sets a new checkpoint
+//	void setNewCheckpoint(GameObject cpnt){
+//		Destroy (cpnt);
+//		setCheckpoint ();				
+//	}
+
+
+	// edits the terrain according to the radius that is set.
 	void editTerrain(){
 		// Take the resolution of the terrain as the boundaries
 		xResolution = Arena.terrainData.heightmapWidth;
@@ -32,7 +49,7 @@ public class Level : MonoBehaviour {
 		// Loop through all values and create a circle with the set radius
 		for (int z = -zResolution/2; z < zResolution/2; z++) {
 			for(int x = -xResolution/2; x < xResolution/2; x++){
-				if(Mathf.Sqrt(Mathf.Pow(x ,2) + Mathf.Pow(z ,2)) >= radius){
+				if(Mathf.Sqrt(Mathf.Pow(x ,2) + Mathf.Pow(z ,2)) >= terrainRadius){
 					heights[x + xResolution/2,z + zResolution/2] = 0.5f;
 				}
 				else
@@ -41,22 +58,4 @@ public class Level : MonoBehaviour {
 		}
 		Arena.terrainData.SetHeights (0, 0, heights);
 	}
-	
-	void adjustTerrainRadius(){
-		if (makeLarger) {
-			if(radius < 200) {
-				radius += Time.deltaTime * 5;
-			} else {
-				makeLarger = false;
-			}
-		} else {
-			if(radius > 150) {
-				radius -= Time.deltaTime * 5;
-			} else {
-				makeLarger = true;
-			}
-		}
-		editTerrain ();
-	}
-
 }
