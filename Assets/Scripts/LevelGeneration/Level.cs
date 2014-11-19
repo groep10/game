2a23234bfd7bd88f5939c9e39b0b3a9ed4c29126
janceesvansenden <jancees_van_senden@hotmail.com>
2014-11-19 15:17:09 +0100

    Created arena generation and adjustment script

--- Assets/Scripts/LevelGeneration/Level.cs
+++ Assets/Scripts/LevelGeneration/Level.cs
@@ -6,24 +6,57 @@ public class Level : MonoBehaviour {
 	public Terrain Arena;
 	public int xResolution;
 	public int zResolution;
+	public bool makeLarger;
+	public float radius = 200;
 
-	public int x = 0;
-
-	// x range is 140 to 370
+	void Start(){
+		// creates an arena terrain with radius 200
+		editTerrain ();
+	}
 
 	// Use this for initialization
 	void Update () {
-		editTerrain();	
+		// makes the arena subsequently smaller and larger with a radius ranging between 150 and 200
+		adjustTerrainRadius ();
+	}
+
+	void FixedUpdate(){
 	}
 
 	void editTerrain(){
+		// Take the resolution of the terrain as the boundaries
 		xResolution = Arena.terrainData.heightmapWidth;
 		zResolution = Arena.terrainData.heightmapHeight;
+		// Retrieve the heightmap of the terrain
 		float[,] heights = Arena.terrainData.GetHeights (0, 0, xResolution, zResolution);
-		for (int z = 0; z < zResolution; z++) {
-			heights[x,z] = 0.5f;
+		// Loop through all values and create a circle with the set radius
+		for (int z = -zResolution/2; z < zResolution/2; z++) {
+			for(int x = -xResolution/2; x < xResolution/2; x++){
+				if(Mathf.Sqrt(Mathf.Pow(x ,2) + Mathf.Pow(z ,2)) >= radius){
+					heights[x + xResolution/2,z + zResolution/2] = 0.5f;
+				}
+				else
+					heights[x + xResolution/2, z + zResolution/2] = 0;
+			}
 		}
-
-		//Arena.terrainData.SetHeights (0, 0, heights);
+		Arena.terrainData.SetHeights (0, 0, heights);
 	}
+	
+	void adjustTerrainRadius(){
+		if (makeLarger) {
+			if(radius < 200) {
+				radius += Time.deltaTime * 5;
+			} else {
+				makeLarger = false;
+			}
+		} else {
+			if(radius > 150) {
+				radius -= Time.deltaTime * 5;
+			} else {
+				makeLarger = true;
+			}
+		}
+		editTerrain ();
+	}
+
 }
