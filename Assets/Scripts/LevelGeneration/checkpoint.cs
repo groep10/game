bac1946b128334faabde52e49d19cbd09ea52a8f
janceesvansenden <jancees_van_senden@hotmail.com>
2014-11-21 17:25:46 +0100

    Added some particle effects helix

--- Assets/Scripts/LevelGeneration/checkpoint.cs
+++ Assets/Scripts/LevelGeneration/checkpoint.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class checkpoint : MonoBehaviour {
 
-	public GameObject level;
-	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(new Vector3(45, 45, 45) * Time.deltaTime);
