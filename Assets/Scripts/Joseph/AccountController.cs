faab230f15c8d67c4d87dabfa368637752971428
Joseph Verburg <josephverburg@gmail.com>
2014-11-26 16:18:47 +0100

    Code

---
+++ Assets/Scripts/Joseph/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class AccountController {
+
+    private static AccountController instance;
+    public static AccountController getInstance() {
+        if (instance == null)
+        {
+            instance = new AccountController();
+        }
+        return instance;
+    }
+
+    public String register(String email, String password) {
+
+        String message = null;
+        WWWForm form = new WWWForm();
+        form.AddField("email", email);
+        form.AddField("password", password);
+        WWW request = new WWW("http://so.meaglin.com/api.php?action=register", form);
+        
+        
+        return message;
+    }
+}
+
