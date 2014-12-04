f6432853997de4ea89f87922bff96ab3d6609a45
Joseph Verburg <josephverburg@gmail.com>
2014-12-04 15:03:33 +0100

    First working login including simple avatar image.

---
+++ Assets/Resources/Scripts/Joseph/Http.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Http
+{
+
+    private static Empty ServerObject
+    {
+        get
+        {
+            if (_serverObject == null)
+            {
+                var gameObj = new GameObject("Server Object");
+                _serverObject = gameObj.AddComponent<Empty>();
+                //GameObject.DontDestroyOnLoad(_serverObject); // Optional
+            }
+            return _serverObject;
+        }
+    }
+    private static Empty _serverObject;
+
+    private String url;
+    private WWWForm form;
+
+    public Http(String url)
+    {
+        this.url = url;
+    }
+
+    public Http(String url, WWWForm form)
+    {
+        this.url = url;
+        this.form = form;
+    }
+
+    public delegate void handleString(String result);
+    public delegate void handleWWW(WWW www);
+
+    public void getText(handleString callback)
+    {
+        ServerObject.StartCoroutine(
+            doRequest(www => {
+                callback(www.text);
+            })
+        );
+    }
+
+    public void getData(handleWWW callback)
+    {
+        ServerObject.StartCoroutine(
+            doRequest(callback)
+        );
+    }
+
+    IEnumerator doRequest(handleWWW callback)
+    {
+        WWW www;
+        if (form != null) {
+            www = new WWW(url, form);
+        } else {
+            www = new WWW(url);
+        }
+        yield return www;
+
+        callback(www);
+    }
+}
