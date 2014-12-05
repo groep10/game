b333e2cdd87ffa51a08039352ae4167fe5dd1932
janceesvansenden <jancees_van_senden@hotmail.com>
2014-12-05 16:03:17 +0100

    Started work on genetic algorithm for checkpoint placement

--- Assets/Resources/Scripts/LevelGeneration/Level.cs
+++ Assets/Resources/Scripts/LevelGeneration/Level.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Level : MonoBehaviour {
 
@@ -9,18 +10,7 @@ public class Level : MonoBehaviour {
 	public bool makeLarger;
 	public float terrainRadius = 200;
 	public GameObject checkpoint;
-
 	private float checkpointTimer = 60;
-
-	// sets the checkpoint in the arena
-	void setCheckpoint(){
-		float x = Random.Range (-300, 300);
-		float z = Random.Range (-300, 300);
-		Vector3 location = new Vector3(x, 50f, z);
-		GameObject cpnt = (GameObject) Instantiate (checkpoint, location, Quaternion.identity);
-		Destroy (cpnt, checkpointTimer);
-		Invoke("setCheckpoint", checkpointTimer);
-	}
 
 	// edits the terrain according to the radius that is set.
 	void editTerrain(){
@@ -41,6 +31,73 @@ public class Level : MonoBehaviour {
 		}
 		Arena.terrainData.SetHeights (0, 0, heights);
 	}
+
+	// sets the checkpoint in the arena
+	void setCheckpoint(){
+		float x = Random.Range (-300, 300);
+		float z = Random.Range (-300, 300);
+		Vector3 location = new Vector3(x, 50f, z);
+		GameObject cpnt = (GameObject) Instantiate (checkpoint, location, Quaternion.identity);
+		Destroy (cpnt, checkpointTimer);
+		Invoke("setCheckpoint", checkpointTimer);
+	}
+
+	/* ------------ GENETIC ALGORITHM TO PLACE THE CHECKPOINT -------------------------- */
+	// Genetic Algorithm variables
+	private int amountOfChromosomes = 30;
+
+	// returns a  Vector2 chromosome representing the possible x and y coordinates of the checkpoint
+	Vector2 createChromosome(){
+		float x = Random.Range (-300, 300);
+		float z = Random.Range (-300, 300);
+		Vector2 result = new Vector2 (x, z);
+		return result;
+	}
+
+	// returns a generation of coordian
+	List<Vector2> createFirstGeneration(){
+		List<Vector2> result = new List<Vector2>();
+		for (int i = 0; i < amountOfChromosomes; i++){
+			result.Add(createChromosome());
+		}
+		return result;
+	}
+
+	// mutates the chromosome by setting either the x or the z value randomly within a certain range
+	void mutate(Vector2 chrom){
+		float chance = Random.Range (0f, 1f);
+		if (chance >= 0.5){
+			chrom.x = Random.Range (-300, 300);
+		}
+		else{
+			chrom.y = Random.Range(-300, 300);
+		}
+	}
+
+	// replace 2 parent chromosomes with 2 child chromosomes using crossover mixing their coordinates
+	void crossover(Vector2 chrom1, Vector2 chrom2){
+		// coordinates
+		float x1 = chrom1.x;
+		float z1 = chrom1.y;
+		float x2 = chrom2.x;
+		float z2 = chrom2.y;
+
+		// create children
+		Vector2 child1 = new Vector2 (x1, z2);
+		Vector2 child2 = new Vector2 (x2, z1);
+
+		// replace parents with children
+		chrom1 = child1;
+		chrom2 = child2;
+	}
+
+
+
+
+
+
+	/* ---------------- END OF GENETIC ALGORITHM ------------------------------ */
+
 
 	void Start(){
 		// creates an arena terrain with radius 200
