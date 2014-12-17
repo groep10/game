b8b046c1a68ebad2a8c0b0f31894d6bb30e4d2ee
Rick Koster <rick_koster12@hotmail.com>
2014-12-17 13:56:17 +0100

    Minimaal script

---
+++ Assets/Resources/Scripts/Rick/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float MotorForce;
+	public WheelCollider WheelColFR;
+	public WheelCollider WheelColFL;
+	public WheelCollider WheelColRR;
+	public WheelCollider WheelColRL;
+	public float SteerForce;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float v = Input.GetAxis ("Vertical") * MotorForce;
+		float h = Input.GetAxis ("Horizontal") * SteerForce;
+
+		WheelColRL.motorTorque = v;
+		WheelColRR.motorTorque = v;
+
+		WheelColFL.steerAngle = h;
+		WheelColFR.steerAngle = h;
+
+	
+	}
+}
