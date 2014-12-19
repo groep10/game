7bbfd9c5ec0298a320f569d7fe5e696a9a3bb128
janceesvansenden <jancees_van_senden@hotmail.com>
2014-12-19 16:49:38 +0100

    Added a reset button, which is the "R" button

--- Assets/Resources/Scripts/Game/respawn.cs
+++ Assets/Resources/Scripts/Game/respawn.cs
@@ -5,20 +5,37 @@ public class respawn : MonoBehaviour {
 
 	private bool upsideDown = false;
 
-	void spawnPlayer(){
-		transform.rotation = Quaternion.identity;
+	// rotates the player up again.
+	void resetRotation(){
+		float x = transform.rotation.eulerAngles.x;
+		float y = transform.rotation.eulerAngles.y;
+
+		transform.rotation = Quaternion.Euler(x, y, 0f);
 		upsideDown = false;
+	}
+
+	// resets the player to a random location within the arena
+	void resetPlayer(){
+		float x = Random.Range (-500, 500);
+		float z = Random.Range (-500, 500);
+
+		transform.position = new Vector3 (x, 3f, z);
+		transform.rotation = Quaternion.identity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.rotation.eulerAngles.z > 120 && transform.rotation.eulerAngles.z < 240) {
+		if (transform.rotation.eulerAngles.z > 90 && transform.rotation.eulerAngles.z < 270) {
 			if(!upsideDown){
 				upsideDown = true;
 				//Debug.Log("Player is upside-down");
-				Invoke("spawnPlayer", 3);
+				Invoke("resetRotation", 3);
 			}
 		}
-	
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			Debug.Log("Resetting player....");
+			Invoke("resetPlayer", 5);
+		}	
 	}
 }
