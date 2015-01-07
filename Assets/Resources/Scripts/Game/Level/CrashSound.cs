6724740144741bcd520efe08d2a1930f882a7ac2
Koen Ziere <Koen@wlan-145-94-187-146.wlan.tudelft.nl>
2015-01-07 17:17:48 +0100

    sounds improved

--- Assets/Resources/Scripts/Game/Level/CrashSound.cs
+++ Assets/Resources/Scripts/Game/Level/CrashSound.cs
@@ -5,9 +5,10 @@ public class CrashSound : MonoBehaviour {
 	
 	void OnCollisionEnter(Collision other) {
 		if(other.gameObject.tag == "Player"){
-			if (other.relativeVelocity.magnitude > 4){
-				audio.Play ();
-			}
+			float volume = (other.relativeVelocity.magnitude / 100);
+			audio.Play ();
+			audio.volume = volume;
+			Debug.Log (volume);
 		}
 	}
 }
