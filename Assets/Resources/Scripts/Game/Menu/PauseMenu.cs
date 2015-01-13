268042624c330e4a3144f87006e8c54bf8cfe7bc
Koen Ziere <Koen@wlan-145-94-187-216.wlan.tudelft.nl>
2015-01-13 11:21:12 +0100

    pause update

--- Assets/Resources/Scripts/Game/Menu/PauseMenu.cs
+++ Assets/Resources/Scripts/Game/Menu/PauseMenu.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+						Debug.Log ("esc is pressed");
+						GameObject.Find ("PauseMenuObjects").SetActive (true);
+				}
+		else
+			GameObject.Find ("PauseMenuObjects").SetActive (false);
+
 	}
 }
