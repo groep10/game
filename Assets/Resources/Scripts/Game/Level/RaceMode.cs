a9ce068e9373872696d096e0bfea6ddda74dd0fc
JanCees van Senden <jancees.van.senden@gmail.com>
2015-01-13 12:31:12 +0100

    Fixed basic racescores

--- Assets/Resources/Scripts/Game/Level/RaceMode.cs
+++ Assets/Resources/Scripts/Game/Level/RaceMode.cs
@@ -27,6 +27,7 @@ namespace Game.Level {
 
 			Debug.Log("Starting Race");
 
+			Game.Controller.getInstance().scores.initializeRaceScores();
 			Game.Controller.getInstance().scores.updateRaceScores();
 
 			if (Network.isServer) {
