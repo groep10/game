27c7e0e92991f5898fc6b7606047fe2fe3ec1bd0
Koen Ziere <Koen@Koens-MacBook-Pro.local>
2015-01-21 15:00:21 +0100

    crash sound update

--- Assets/Resources/Scripts/Game/Level/CrashSound.cs
+++ Assets/Resources/Scripts/Game/Level/CrashSound.cs
@@ -4,11 +4,10 @@ using System.Collections;
 public class CrashSound : MonoBehaviour {
 	
 	void OnCollisionEnter(Collision other) {
-		if(other.gameObject.tag == "Player"){
+		if(other.gameObject.tag == "ArenaAssets"){
 			float volume = (other.relativeVelocity.magnitude / 100);
 			audio.Play ();
 			audio.volume = volume;
-			Debug.Log (volume);
 		}
 	}
 }
