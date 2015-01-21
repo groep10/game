9d3f59d425817c1a28a13faf2420608840355616
Joseph Verburg <josephverburg@gmail.com>
2015-01-21 16:23:01 +0100

    Code cleanup, proper namespacing.

---
+++ Assets/Resources/Scripts/Game/Net/NetworkSeed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Game.Level;
+
+namespace Game.Net {
+	public class NetworkSeed : MonoBehaviour {
+
+		public int syncSeed;
+		// Use this for initialization
+		void Start () {
+			if (Network.isServer) {
+				int seed = Random.Range (0, 100);
+				networkView.RPC("setSeed", RPCMode.AllBuffered, seed);
+			}
+		}
+
+		[RPC]
+		void setSeed(int seed) {
+			syncSeed = seed;
+			transform.GetComponentInChildren<FencePlacement> ().onStart (syncSeed);
+		}
+
+
+	}
+}
